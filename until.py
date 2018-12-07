c15d9377cace764390aa4badde81c45d8b4eacf5
Deylies <admin@example.com>
2018-12-07 18:20:28 +0800

    app元类/父类的初步代

--- until.py
+++ until.py
@@ -8,19 +8,24 @@ class Conf(dict):
         读取配置文件的基础功能封装，通过传入
         :param conf_name: 配置文件名称
         :return: 配置文件的json
-        >>> conf_app = Conf('conf_app.json')
+        >>> conf_app = Conf("conf_program.json")
         >>> print(conf_app.keys)
         dict_keys(['init_dir'])
         >>> conf_app.desc()
         name:init_dir type:<class 'list'> size:6
     """
+
     def __init__(self, conf_name):
         super().__init__()
         self.name = conf_name
-        with open(os.path.join('conf',conf_name),'r') as f:
+        with open(os.path.join('conf', conf_name), 'r') as f:
             self.__json = json.load(f)
         self.__keys = self.__json.keys()
-        self.__desc = '\n'.join(['name:%s type:%s size:%s'%(k,type(v),len(v)) for k,v in self.__json.items()])
+        self.__desc = []
+        for k, v in self.__json.items():
+            self[k] = v
+            self.__desc.append('name:%s type:%s size:%s' % (k, type(v), len(v)))
+        self.__desc = '\n'.join(self.__desc)
 
     @property
     def keys(self):
@@ -29,7 +34,8 @@ class Conf(dict):
     def desc(self):
         print(self.__desc)
 
+
 if __name__ == '__main__':
     import doctest
-    doctest.testmod()
 
+    doctest.testmod()
--- weblife.py
+++ weblife.py
@@ -1,3 +1,15 @@
 # encoding:utf-8
 # __author__:DeyLies,WangYu
-import os,json
+import os
+from until import Conf
+
+def init():
+    PROG_CONF = Conf("conf_program.json")
+    print(PROG_CONF)
+    for fd in PROG_CONF['init_dir']:
+        if not os.path.exists(fd):
+            os.mkdir(fd)
+
+
+if __name__ == '__main__':
+    init()
